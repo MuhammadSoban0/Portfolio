5bd1fe7fa06d8773377dc66d0c847d1ca83474c8
MuhammadSoban0 <sobanhannan654@gmail.com>
2026-02-06 14:21:36 +0500

    Use profile image as favicon

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import profileIcon from "@/assets/profile.png";
+
+const existingIcon = document.querySelector<HTMLLinkElement>("link[rel*='icon']");
+const favicon = existingIcon ?? document.createElement("link");
+favicon.rel = "icon";
+favicon.href = profileIcon;
+if (!existingIcon) document.head.appendChild(favicon);
 
 createRoot(document.getElementById("root")!).render(<App />);
